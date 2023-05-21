28154555f49855af70730f13d78acf07c48e3ec3
Navid jaberi <133659142+briofy-navidjaberi@users.noreply.github.com>
2023-05-21 19:54:56 +0330

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -8,6 +8,9 @@ interface IHeader {
   megaMenu: boolean | string[];
   hideSearchBar: boolean;
   hideLanguageSelector: boolean;
+  hideNotifications: boolean;
+  hideappMenu: boolean;
+  hideUserInfo: boolean;
   languageList: {
     lang: string;
     flagSrc: string;
@@ -41,7 +44,35 @@ interface IFooter {
     link: string;
   }[];
 }
+interface IAnnouncement {
+  title: string;
+  badge: string;
+  link: ILink;
+}
+interface ILink {
+  text: string;
+  src: string;
+}
+interface IUserData {
+  avatar: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  email: string;
+}
+interface IAppMenu {
+  iconName: string;
+  iconTitle: string;
+  link: string;
+}
+interface UserAvatarMenu {
+  accountLinks: AccountLink[];
+}
 
+interface AccountLink {
+  title: string;
+  link: string;
+}
 export default defineAppConfig({
   nuxtIcon: {
     // Default size
@@ -60,6 +91,9 @@ export default defineAppConfig({
       hideLanguageSelector: false,
       languageList: [],
       hideThemeSelector: false,
+      hideNotifications: false,
+      hideappMenu: false,
+      hideUserInfo: false,
       loginButton: {
         title: "Login",
         hideLoginButton: false,
@@ -88,5 +122,66 @@ export default defineAppConfig({
         >Briofy</a> Family </p>`,
       socials: [],
     } as IFooter,
+    announcement: {
+      title: "Hello ! here is the announcement test from the Trader4",
+      badge: "new",
+      link: {
+        text: "Homepage",
+        src: "/",
+      },
+    } as IAnnouncement,
+    userData: {
+      avatar: "",
+    } as IUserData,
+
+    appsMenu: [
+      {
+        iconName: "mdi-shopping",
+        iconTitle: "Sales",
+        link: "#",
+      },
+      {
+        iconName: "mdi-account-group",
+        iconTitle: "Users",
+        link: "#",
+      },
+      {
+        iconName: "mdi-inbox",
+        iconTitle: "Inbox",
+        link: "#",
+      },
+      {
+        iconName: "mdi-account-circle",
+        iconTitle: "Profile",
+        link: "#",
+      },
+      {
+        iconName: "mdi-cog",
+        iconTitle: "Settings",
+        link: "#",
+      },
+      {
+        iconName: "mdi-bitbucket",
+        iconTitle: "Products",
+        link: "#",
+      },
+      {
+        iconName: "mdi-currency-usd-circle",
+        iconTitle: "Pricing",
+        link: "#",
+      },
+      { iconName: "mdi-percent-box", iconTitle: "Billing", link: "#" },
+      {
+        iconName: "mdi-logout-variant",
+        iconTitle: "Logout",
+        link: "#",
+      },
+    ] as IAppMenu[],
+    userAvatarMenu: {
+      accountLinks: [
+        { title: "My profile", link: "#" },
+        { title: "Account settings", link: "#" },
+      ],
+    } as UserAvatarMenu,
   },
 });
