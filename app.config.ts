4ba4d4136ed46a5d7e69d86afe39905b0fb16c6e
briofy-mammad <m.ranjbar@briofy.net>
2023-06-04 11:29:46 +0330

    [ADD] interface for headerLinks

--- app.config.ts
+++ app.config.ts
@@ -18,6 +18,13 @@ interface IAppConfig {
   };
 }
 
+interface IHeaderLinks {
+  icon?: string;
+  title: string;
+  link: string;
+  submenu?: IHeaderLinks;
+}
+
 interface IHeader {
   logo: string;
   darkLogo: string;
@@ -25,16 +32,7 @@ interface IHeader {
     title: string;
     link: string;
   }[];
-  subnavLinks?: {
-    icon: string;
-    title: string;
-    link: string;
-    submenu?: {
-      icon: string;
-      title: string;
-      link: string;
-    }[];
-  }[];
+  subnavLinks?: IHeaderLinks[];
   megaMenu: boolean | string[];
   hideSubnavLink?: boolean;
   hideSearchBar: boolean;
