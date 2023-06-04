6901bc70329a806d2154eff28d938c8154d8dc16
briofy-mammad <m.ranjbar@briofy.net>
2023-06-04 08:43:38 +0330

    [FIX] closing menu when route changes

--- components/BaseDropDown.vue
+++ components/BaseDropDown.vue
@@ -19,24 +19,44 @@
       aria-labelledby="dropdownDefaultButton"
     >
       <li v-for="(subItem, subIndex) in submenu">
-        <NuxtLink
-          :to="subItem.link"
-          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+        <a
+          @click="changeRoute(subItem.link)"
+          class="block px-4 cursor-pointer py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
           aria-current="page"
           active-class="!text-blue-700 dark:!text-blue-500 border-b-2 border-blue-600 
               dark:border-blue-500"
         >
           {{ subItem.title }}
-        </NuxtLink>
+        </a>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { Dropdown } from "flowbite";
+
+const localePath = useLocalePath();
+const router = useRouter();
+const props = defineProps<{
   title: string;
   icon: string;
   submenu: [] | any;
 }>();
+
+const changeRoute = (link: string) => {
+  router.push(localePath(link));
+  /*
+   * $targetEl: required
+   * $triggerEl: required
+   * options: optional
+   */
+  // set the dropdown menu element
+  const $targetEl = document.getElementById(`dropdown-${props.title}`);
+  // set the element that trigger the dropdown menu on click
+  const $triggerEl = document.getElementById(`sub-${props.title}`);
+  const dropdown = new Dropdown($targetEl, $triggerEl);
+  // hide the dropdown menu
+  dropdown.hide();
+};
 </script>
